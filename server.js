f0380f7de06d2bd873edd3eec89b8e78759311e0
Lestibournes <sher.singh.619@gmail.com>
2018-11-06 18:05:19 +0530

    Reset Button Fix 3

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.post('/', (req, res) => {
         }
     });
 });
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log("Listening on port 3000"));
+app.listen(PORT, () => console.log("Listening on port, up and running!"));
